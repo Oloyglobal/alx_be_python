3c7d7305fcd8e19c34e9f62d4b9d1ea62ebda1f4
O'loy Global <oloyedeolaniyi22@gmail.com>
2025-09-12 17:13:37 +0100

    Add basic_operations.py for Python introduction task

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,19 @@
+# number1 = 10
+# number2 = 5
+# some_result = number1 + number2
+# difference_result = number1 - number2
+# product_result = number1 * number2
+# print(some_result)
+# print(difference_result)
+# print(product_result)
+
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+print(f"sum of {number1} and {number2} is {sum_result}")
+print(f"difference of {number1} and {number2} is {difference_result}")
+print(f"product of {number1} and {number2} is {product_result}")
